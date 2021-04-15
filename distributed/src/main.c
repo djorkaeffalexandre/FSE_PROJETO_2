568bfcdb518a9fc0c164af280a0d572ccc14d7e8
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2021-04-15 20:59:09 -0300

    BME280

--- distributed/src/main.c
+++ distributed/src/main.c
@@ -11,10 +11,10 @@
 int main() {
 	gpio_init();
 	quit_init();
-	// bme280_init();
+	bme280_init();
 
-	// struct bme280_data data = bme280_read();
-	// printf("%4.2f %4.2f\n", data.temperature, data.humidity);
+	struct bme280_data data = bme280_read();
+	printf("%4.2f %4.2f\n", data.temperature, data.humidity);
 
 	pthread_t server;
 
