8d4ba4a8dea018562817cf168f5b4b34717ea0e2
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2021-04-15 21:36:18 -0300

    Use yellow as color

--- central/src/menu.c
+++ central/src/menu.c
@@ -104,7 +104,7 @@ void* menu_handler() {
 	noecho();
 	cbreak();
 	curs_set(0);
-	init_pair(1, COLOR_BLUE, COLOR_BLACK);
+	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
 	init_pair(2, COLOR_RED, COLOR_BLACK);
 	init_pair(3, COLOR_GREEN, COLOR_BLACK);
      
